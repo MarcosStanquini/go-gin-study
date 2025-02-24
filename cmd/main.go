f123ff3df29cf98c93ffcfc9172f92dcc7b1baec
MarcosStanquini <marcossnarquini@gmail.com>
2025-02-24 00:09:04 -0300

    feat: learn about swagger

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"go-api/controller"
-	"go-api/db"
-	"go-api/repository"
-	"go-api/usecase"
-	"github.com/gin-gonic/gin"
+    _ "go-api/cmd/docs"
+    "go-api/controller"
+    "go-api/db"
+    "go-api/repository"
+    "go-api/usecase"
+    "github.com/gin-gonic/gin"
+    "github.com/swaggo/gin-swagger"
+    "github.com/swaggo/files"
 )
 
+
+
 func main() {
-	server := gin.Default()
-	dbConnection, err := db.ConnectDB()
-	if err != nil {
-		panic(err)
-	}
+    server := gin.Default()
+    dbConnection, err := db.ConnectDB()
+    if err != nil {
+        panic(err)
+    }
 
 	ProductRepository := repository.NewProductRepository(dbConnection)
 
@@ -25,5 +30,6 @@ func main() {
 	server.GET("/product/:productId", ProductController.GetProductsByID)
 	server.DELETE("/product/:productId", ProductController.DeleteProduct)
 
-	server.Run(":8000")
+    server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+    server.Run(":8000")
 }
--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,11 +1,11 @@
 package controller
 
 import (
+	"database/sql"
 	"go-api/model"
 	"go-api/usecase"
 	"net/http"
 	"strconv"
-	"database/sql"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,6 +20,14 @@ func NewProductController(usecase usecase.ProductUseCase) ProductController {
 	}
 }
 
+// @Summary Get all products
+// @Description Get all products
+// @Tags products
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} model.Product
+// @Failure 500 {object} model.Response
+// @Router /products [get]
 func (p *ProductController) GetProduct(ctx *gin.Context) {
 	products, err := p.productUseCase.GetProducts()
 	if err != nil {
@@ -29,6 +37,16 @@ func (p *ProductController) GetProduct(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, products)
 }
 
+// @Summary Create a new product
+// @Description Create a new product
+// @Tags products
+// @Accept  json
+// @Produce  json
+// @Param product body model.Product true "Product to create"
+// @Success 201 {object} model.Product
+// @Failure 400 {object} model.Response
+// @Failure 500 {object} model.Response
+// @Router /product [post]
 func (p *ProductController) CreateProduct(ctx *gin.Context) {
 	var product model.Product
 	err := ctx.BindJSON(&product)
@@ -44,6 +62,17 @@ func (p *ProductController) CreateProduct(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, insertedProduct)
 }
 
+// @Summary Get product by ID
+// @Description Get product by ID
+// @Tags products
+// @Accept  json
+// @Produce  json
+// @Param productId path int true "Product ID"
+// @Success 200 {object} model.Product
+// @Failure 400 {object} model.Response
+// @Failure 404 {object} model.Response
+// @Failure 500 {object} model.Response
+// @Router /product/{productId} [get]
 func (p *ProductController) GetProductsByID(ctx *gin.Context) {
 	id := ctx.Param("productId")
 	if id == "" {
@@ -66,7 +95,7 @@ func (p *ProductController) GetProductsByID(ctx *gin.Context) {
 		return
 	}
 
-	if(product == nil){
+	if product == nil {
 		response := model.Response{
 			Message: "Produto não encontrado na base de dados",
 		}
@@ -78,39 +107,48 @@ func (p *ProductController) GetProductsByID(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, product)
 }
 
+// @Summary Delete product by ID
+// @Description Delete product by ID
+// @Tags products
+// @Accept  json
+// @Produce  json
+// @Param productId path int true "Product ID"
+// @Success 200 {object} model.Response
+// @Failure 400 {object} model.Response
+// @Failure 404 {object} model.Response
+// @Failure 500 {object} model.Response
+// @Router /product/{productId} [delete]
 func (p *ProductController) DeleteProduct(ctx *gin.Context) {
-    id := ctx.Param("productId")
-    if id == "" {
-        response := model.Response{
-            Message: "Id do produto não pode ser nulo",
-        }
-        ctx.JSON(http.StatusBadRequest, response)
-        return
-    }
+	id := ctx.Param("productId")
+	if id == "" {
+		response := model.Response{
+			Message: "Id do produto não pode ser nulo",
+		}
+		ctx.JSON(http.StatusBadRequest, response)
+		return
+	}
 
-    productID, err := strconv.Atoi(id)
-    if err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{"error": "Id do produto precisa ser um número"})
-        return
-    }
+	productID, err := strconv.Atoi(id)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Id do produto precisa ser um número"})
+		return
+	}
 
-    err = p.productUseCase.DeleteProduct(productID)
-    if err != nil {
-        if err == sql.ErrNoRows {
-            response := model.Response{
-                Message: "Produto não encontrado na base de dados",
-            }
-            ctx.JSON(http.StatusNotFound, response)
-        } else {
-            ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        }
-        return
-    }
+	err = p.productUseCase.DeleteProduct(productID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			response := model.Response{
+				Message: "Produto não encontrado na base de dados",
+			}
+			ctx.JSON(http.StatusNotFound, response)
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
 
-    response := model.Response{
-        Message: "Produto deletado com sucesso",
-    }
-    ctx.JSON(http.StatusOK, response)
+	response := model.Response{
+		Message: "Produto deletado com sucesso",
+	}
+	ctx.JSON(http.StatusOK, response)
 }
-
-
